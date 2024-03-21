862151a84571d823453b1a73da3f372c659c1fe0
Айнур Якупов <yak.ainur@yandex.ru>
2024-03-22 00:41:21 +0300

    laba-3 task-3

--- src/server.js
+++ src/server.js
@@ -1,18 +1,15 @@
-import http from 'http';
-import fs from 'fs';
+import express from 'express';
 
-const server = http.createServer((req, res) => {
-    fs.readFile('./task/tasks.json', 'utf8', (err, data) => {
-        if (err) {
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end("Error reading file.");
-            return;
-        }
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(data);
-    });
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
 });
 
-server.listen(3000, () => {
-    console.log("Server running at http://localhost:3000/");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
